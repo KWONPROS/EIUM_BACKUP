ece9572a03024e04d551a84e3d73dd7d33c06675
song <won0935@naver.com>
2019-10-21 12:50:35 +0900

    머멈

---
+++ EIUM/src/main/java/com/myspring/pro27/util/dao/FindIdNPwdDAO.java
@@ -0,0 +1,11 @@
+package com.myspring.pro27.util.dao;
+
+import com.myspring.pro27.util.vo.FindIdNPwdVO;
+
+
+public interface FindIdNPwdDAO {
+
+	public FindIdNPwdVO findId(FindIdNPwdVO findIdNPwdVO);
+	public FindIdNPwdVO findPwd(FindIdNPwdVO findIdNPwdVO);
+
+}
---
+++ EIUM/src/main/java/com/myspring/pro27/util/dao/FindIdNPwdDAOImpl.java
@@ -0,0 +1,24 @@
+package com.myspring.pro27.util.dao;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.myspring.pro27.util.vo.FindIdNPwdVO;
+
+@Repository("FindIdNPwdDAO")
+public class FindIdNPwdDAOImpl implements FindIdNPwdDAO {
+	@Autowired
+	private SqlSession sqlSession;
+
+	public FindIdNPwdVO findId(FindIdNPwdVO findIdNPwdVO) {
+		findIdNPwdVO = sqlSession.selectOne("mapper.util.findId", findIdNPwdVO);
+		return findIdNPwdVO;
+	}
+	public FindIdNPwdVO findPwd(FindIdNPwdVO findIdNPwdVO) {
+		findIdNPwdVO = sqlSession.selectOne("mapper.util.findId", findIdNPwdVO);
+		return findIdNPwdVO;
+	}
+	
+
+}
